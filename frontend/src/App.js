5ba47621ba2a96fa715a40f1b049950076edb39f
Jukka Pekka <jukka@DESKTOP-CF449NH.localdomain>
2022-07-17 12:30:10 +0300

    front and backend initialized

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,30 @@
-
+import { useState } from 'react'
+import axios from 'axios'
 import './App.css';
 
 function App() {
+  const [profileData, setProfileData] = useState([])
+
+
+  axios({
+    method: 'GET',
+    url: "/profile",
+  }).then((response) => {
+    console.log(response)
+    const res = response.data;
+    setProfileData(({
+      name: res.name,
+      about: res.about
+    }))
+    console.log(profileData)
+  }).catch((error) => {
+    console.log(error.response)
+  })
+
   return (
     <div className="App">
       <p>
-        Hi
+        Hi {profileData.name} {profileData.about}
       </p>
     </div>
   );
