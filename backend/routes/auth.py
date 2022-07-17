d739b5bb376461d75af4cc296a71b826e6f8f106
Jukka Pekka <jukka@DESKTOP-CF449NH.localdomain>
2022-07-17 13:50:49 +0300

    J

--- backend/routes/auth.py
+++ backend/routes/auth.py
@@ -9,7 +9,7 @@ from flask_jwt_extended import create_access_token, get_jwt, get_jwt_identity, \
 auth = Blueprint('auth', __name__)
 
 
-@auth.route('/token')
+@auth.route('/token', methods=['POST'])
 def create_token():
     email = request.json.get("email", None)
     password = request.json.get("password", None)
